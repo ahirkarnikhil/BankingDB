94639a1a84589b89a92c377c66604210c862a973
ahirkarnikhil <ahirkarnikhil35@gmail.com>
2026-07-29 09:18:12 +0530

    created new tables

--- banking.sql
+++ banking.sql
@@ -11,3 +11,38 @@ AccountCreationDate date
 );
 
 describe customers;
+create table Accounts (
+AccountID int,
+AccountType varchar(20),
+Balance decimal(10,2)
+);
+desc accounts;
+
+create table Transactions (
+TransactionID int,
+TransactionDate date,
+Amount decimal(10,2),
+TransactionType varchar(20)
+);
+
+
+create table Branches (
+BranchID int,
+BranchName varchar(100),
+BranchAddress varchar(200),
+BranchPhone varchar(15)
+);
+
+
+create table AccountBranches(
+AssignmentDate date	
+);
+
+
+create table Loans (
+LoanID int,
+LoanAmount decimal(10,2),
+InterestRate decimal(5,2),
+StratDate date,
+EndDate date
+);
