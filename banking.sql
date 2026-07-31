d0528bdf1a9aa1c87397a24dde569fe6dde30ca0
ahirkarnikhil <ahirkarnikhil35@gmail.com>
2026-07-31 09:21:18 +0530

    table modification

--- banking.sql
+++ banking.sql
@@ -46,3 +46,88 @@ InterestRate decimal(5,2),
 StratDate date,
 EndDate date
 );
+
+alter table customers
+add DateOfBirth Date;
+desc customers;
+
+alter table customers
+modify Phone varchar (20);
+desc customers;
+
+alter table accounts
+add constraint chk_MinBalance
+check(Balance>=1000);
+
+drop table accountbranches;
+
+
+alter table accounts
+add CustomerID int;
+
+alter table accounts
+add constraint FK_Accounts_customers
+foreign key (customerID)
+references customers (customerID);
+
+desc accounts;
+
+alter table accounts
+add constraint 
+ primary key (AccountID);
+ 
+ desc accounts ;
+ 
+ alter table customers
+ modify FristName varchar(50) not null;
+ 
+ alter table customers
+ add constraint uq_Email unique(Email);
+ 
+ alter table branches
+ add constraint 
+ primary key (BranchID);
+ 
+ desc branches;
+ 
+ alter table accounts
+ add branchID int;
+ 
+ alter table accounts
+ add constraint
+ foreign key (branchID)
+ references branches(BranchID);
+ 
+ desc accounts ;
+ 
+ alter table transactions
+ add accountID int;
+ 
+ alter table transactions
+ add constraint
+ foreign key (accountID)
+ references accounts (accountID);
+ 
+ alter table loans
+ add customerID int;
+ 
+ alter table loans
+ add constraint FK_Loans_customerID
+ foreign key (customerID)
+ references customers(customerID);
+ 
+ select
+ constraint_type
+ from information_schema.table_constraints
+ where table_schema = 'bankingdb'
+ and table_name = 'accounts' ;
+
+alter table transactions
+add constraint pk_transactionID_transactions
+primary key (transactionID);
+
+alter table loans
+add constraint pk_loans
+primary key (loanID);
+
+desc transactions;
